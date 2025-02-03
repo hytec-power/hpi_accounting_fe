07218bfa29f7c09a04aefe5b30bcbba6c0586c49
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-04 07:26:36 +0800

    accounting template components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { AccountingTemplateComponent} from "src/app/pages/accounting/common/template/template.component";
 
 export const routes: Routes = [
     {
@@ -7,7 +8,7 @@ export const routes: Routes = [
     },
     {
         path: 'accounting',
-        loadChildren: () => import('./pages/accounting/accounting.module').then(m=>m.AccountingModule)
-
+        loadChildren: () => import('./pages/accounting/accounting.module').then(m=>m.AccountingModule),
+        component: AccountingTemplateComponent
     }
 ];
