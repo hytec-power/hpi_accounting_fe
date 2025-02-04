1d2263eca79fad41f7ebc369116f0e2f49d82e8e
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-04 08:45:24 +0800

    sidebar icons active and inactive states

--- src/app/pages/accounting/common/sidebar/sidebar.component.ts
+++ src/app/pages/accounting/common/sidebar/sidebar.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'accounting-sidebar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-  items:{ name: string,link: string,bi_icon?: string }[] = [];
+  items:{ name: string,link: string, bi_icon?: string , bi_icon_active?: string }[] = [];
   constructor() {
-    this.items.push({name: 'Dashboard', link: '/accounting', bi_icon: 'bi-house' });
-    this.items.push({name: 'Dashboard', link: '' });
+    this.items.push({name: 'Dashboard',
+                     link: '/accounting',
+                     bi_icon: 'bi-house',
+                     bi_icon_active: 'bi-house-door-fill' });
+
+    this.items.push({name: 'Budget Request',
+                     link: '' ,
+                     bi_icon: 'bi-wallet-fill' });
 
   }
 }
