5f469042d874483c2bce5cf1ba0d359387e90aca
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-04 08:23:41 +0800

    accounting sidebar

--- src/app/pages/accounting/common/sidebar/sidebar.component.ts
+++ src/app/pages/accounting/common/sidebar/sidebar.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'accounting-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-  links:{ name: string,link: string,bi_icon?: string }[] = [];
+  items:{ name: string,link: string,bi_icon?: string }[] = [];
   constructor() {
-    this.links.push({name: 'Dashboard', link: '',bi_icon: '' });
-    this.links.push({name: 'Dashboard', link: '',bi_icon: '' });
+    this.items.push({name: 'Dashboard', link: '/accounting', bi_icon: 'bi-house' });
+    this.items.push({name: 'Dashboard', link: '' });
 
   }
 }
