e8cfbea3ec3b72ef9f72693f2cd0be0c10c969b7
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-05 17:46:54 +0800

    page title component

--- src/app/pages/accounting/budget-requests/index/index.component.ts
+++ src/app/pages/accounting/budget-requests/index/index.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {PageTitleComponent} from "src/app/common/page-title/page-title.component";
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+    imports: [
+        PageTitleComponent
+    ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
