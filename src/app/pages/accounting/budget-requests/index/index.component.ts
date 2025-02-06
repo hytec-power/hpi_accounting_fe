c07cfde4ff6fee7047f8afb82c634a72ea7fef66
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-06 10:56:08 +0800

    request id

--- src/app/pages/accounting/budget-requests/index/index.component.ts
+++ src/app/pages/accounting/budget-requests/index/index.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {PageTitleComponent} from "src/app/common/page-title/page-title.component";
 import {ThemeService} from "src/app/services/theme/theme.service";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-index',
   standalone: true,
-    imports: [
-        PageTitleComponent
-    ],
+  imports: [
+    PageTitleComponent,
+    RouterLink
+  ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
