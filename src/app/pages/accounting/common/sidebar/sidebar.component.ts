322baaa2a0516394c79a3b30873f26fa58841c48
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-06 11:08:13 +0800

    sidebar path match

--- src/app/pages/accounting/common/sidebar/sidebar.component.ts
+++ src/app/pages/accounting/common/sidebar/sidebar.component.ts
@@ -13,18 +13,20 @@ import {ThemeService} from "src/app/services/theme/theme.service";
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-  items:{ name: string,link: string, bi_icon?: string , bi_icon_active?: string }[] = [];
+  items:{ name: string,link: string, bi_icon?: string , bi_icon_active?: string ; exact: boolean }[] = [];
   toggle_temp: boolean = true;
   constructor(private theme: ThemeService) {
     this.items.push({name: 'Dashboard',
                      link: '/accounting',
                      bi_icon: 'bi-house',
-                     bi_icon_active: 'bi-house-door-fill' });
+                     bi_icon_active: 'bi-house-door-fill',
+                     exact: true });
 
     this.items.push({name: 'Budget Request',
                      link: '/accounting/budget-requests' ,
                      bi_icon: 'bi-wallet2',
-                     bi_icon_active: 'bi-wallet-fill' });
+                     bi_icon_active: 'bi-wallet-fill',
+                     exact: false });
   }
   test(){
       this.toggle_temp = !this.toggle_temp;
