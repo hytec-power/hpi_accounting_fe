f16a2376f676a5b43b190b042ba2b437b32b0ec9
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-06 13:43:01 +0800

    stepper component dynamic

--- src/app/common/stepper/stepper.component.ts
+++ src/app/common/stepper/stepper.component.ts
@@ -1,14 +1,18 @@
 import {Component, input} from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'stepper-component',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './stepper.component.html',
   styleUrl: './stepper.component.scss'
 })
 export class StepperComponent {
   items = input<StepperItem[]>([]);
+  current_index = input<number>(0);
 }
 export interface StepperItem{
   name: string,
