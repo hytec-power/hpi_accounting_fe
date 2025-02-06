6d716de57ee32aa44737a6e5d439a4704168fc9e
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-06 13:49:34 +0800

    stepper height, details component

---
+++ src/app/pages/accounting/budget-requests/view/request-user-details/request-user-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'request-user-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './request-user-details.component.html',
+  styleUrl: './request-user-details.component.scss'
+})
+export class RequestUserDetailsComponent {
+
+}
--- src/app/pages/accounting/budget-requests/view/view.component.ts
+++ src/app/pages/accounting/budget-requests/view/view.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import {PageTitleComponent} from "src/app/common/page-title/page-title.component";
 import {StepperComponent, StepperItem} from "src/app/common/stepper/stepper.component";
+import {
+  RequestUserDetailsComponent
+} from "src/app/pages/accounting/budget-requests/view/request-user-details/request-user-details.component";
 
 @Component({
   selector: 'view-request',
   standalone: true,
   imports: [
     PageTitleComponent,
-    StepperComponent
+    StepperComponent,
+    RequestUserDetailsComponent
   ],
   templateUrl: './view.component.html',
   styleUrl: './view.component.scss'
