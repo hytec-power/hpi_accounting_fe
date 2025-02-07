bf05bb82ad049faa9ee8cb46efa417058cc6b471
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-07 16:10:42 +0800

    employee interview

--- src/app/pages/employee/employee.module.ts
+++ src/app/pages/employee/employee.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { RouterModule } from "@angular/router";
+import { EmployeeRoutes } from "src/app/pages/employee/route";
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(EmployeeRoutes)
   ]
 })
 export class EmployeeModule { }
---
+++ src/app/pages/employee/route.ts
@@ -0,0 +1,8 @@
+import {Route} from "@angular/router";
+import { DashboardComponent } from "src/app/pages/employee/dashboard/dashboard.component";
+
+export const EmployeeRoutes: Route[]=[
+    { path: '',
+      component: DashboardComponent
+    }
+];
