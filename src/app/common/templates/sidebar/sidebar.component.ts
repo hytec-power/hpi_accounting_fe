01eef7c51da468cbb0f7f3aadcd7b89380196789
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-07 13:44:01 +0800

    init sidebar items

--- src/app/common/templates/sidebar/sidebar.component.ts
+++ src/app/common/templates/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
 import {ThemeService} from "src/app/services/theme/theme.service";
 import {RouterLink, RouterLinkActive} from "@angular/router";
 
@@ -13,20 +13,10 @@ import {RouterLink, RouterLinkActive} from "@angular/router";
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-  items:{ name: string,link: string, bi_icon?: string , bi_icon_active?: string ; exact: boolean }[] = [];
+  items = input<SidebarItem[]>([]);
   toggle_temp: boolean = true;
   constructor(private theme: ThemeService) {
-    this.items.push({name: 'Dashboard',
-      link: '/accounting',
-      bi_icon: 'bi-house',
-      bi_icon_active: 'bi-house-door-fill',
-      exact: true });
 
-    this.items.push({name: 'Budget Request',
-      link: '/accounting/budget-requests' ,
-      bi_icon: 'bi-wallet2',
-      bi_icon_active: 'bi-wallet-fill',
-      exact: false });
   }
   test(){
     this.toggle_temp = !this.toggle_temp;
--- src/app/pages/accounting/common/template/template.component.ts
+++ src/app/pages/accounting/common/template/template.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
 import {HeaderComponent} from "src/app/common/templates/header/header.component";
-import {SidebarComponent} from "src/app/common/templates/sidebar/sidebar.component";
+import {SidebarComponent, SidebarItem} from "src/app/common/templates/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-template',
@@ -15,5 +15,21 @@ import {SidebarComponent} from "src/app/common/templates/sidebar/sidebar.compone
   styleUrl: './template.component.scss'
 })
 export class AccountingTemplateComponent {
+    sidebar_items: SidebarItem[]=[];
+    constructor() {
+        this.initSidebar();
+    }
+    initSidebar(){
+        this.sidebar_items.push({name: 'Dashboard',
+                                 link: '/accounting',
+                                 bi_icon: 'bi-house',
+                                 bi_icon_active: 'bi-house-door-fill',
+                                 exact: true });
 
+        this.sidebar_items.push({name: 'Budget Request',
+                                 link: '/accounting/budget-requests' ,
+                                 bi_icon: 'bi-wallet2',
+                                 bi_icon_active: 'bi-wallet-fill',
+                                 exact: false });
+    }
 }
