1a8a5af3287e5118ed2f11104b2000cc6d89025f
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-07 18:43:28 +0800

    create budget request

--- src/app/pages/employee/budget-requests/view/view.component.ts
+++ src/app/pages/employee/budget-requests/view/view.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {BudgetRequestViewComponent} from "src/app/shared/budget-request/view/view.component";
+import {PageTitleComponent} from "src/app/common/page-title/page-title.component";
 
 @Component({
   selector: 'app-view',
   standalone: true,
-    imports: [
-        BudgetRequestViewComponent
-    ],
+  imports: [
+    BudgetRequestViewComponent,
+    PageTitleComponent
+  ],
   templateUrl: './view.component.html',
   styleUrl: './view.component.scss'
 })
