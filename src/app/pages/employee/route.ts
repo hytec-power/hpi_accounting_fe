cf37af8980ab9f0ea81f9c392eaf82eb18cbdf9a
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-07 16:46:58 +0800

    budget request indx for employee

--- src/app/pages/employee/route.ts
+++ src/app/pages/employee/route.ts
@@ -1,8 +1,12 @@
 import {Route} from "@angular/router";
 import { DashboardComponent } from "src/app/pages/employee/dashboard/dashboard.component";
+import { IndexComponent  as BudgetRequestIndex } from "src/app/pages/employee/budget-requests/index/index.component";
 
 export const EmployeeRoutes: Route[]=[
     { path: '',
       component: DashboardComponent
+    },{
+        path: 'budget-requests',
+        component: BudgetRequestIndex
     }
 ];
