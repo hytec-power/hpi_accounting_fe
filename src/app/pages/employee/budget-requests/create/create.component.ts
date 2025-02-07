fa799f593d2118335b5040481e99bcdd8b89f6d8
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-07 18:34:01 +0800

    budget request editor embed

--- src/app/pages/employee/budget-requests/create/create.component.ts
+++ src/app/pages/employee/budget-requests/create/create.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {EditorComponent} from "src/app/shared/budget-request/editor/editor.component";
+import {PageTitleComponent} from "src/app/common/page-title/page-title.component";
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [
+    EditorComponent,
+    PageTitleComponent
+  ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
