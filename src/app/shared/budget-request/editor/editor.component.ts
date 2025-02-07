91b968650fedbe35bf5fb8d06224998f3673b98f
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-07 18:55:40 +0800

    request details

--- src/app/shared/budget-request/editor/editor.component.ts
+++ src/app/shared/budget-request/editor/editor.component.ts
@@ -1,12 +1,31 @@
 import {Component, input} from '@angular/core';
+import {StepperComponent, StepperItem} from "src/app/common/stepper/stepper.component";
 
 @Component({
   selector: 'budget-request-editor',
   standalone: true,
-  imports: [],
+  imports: [
+    StepperComponent
+  ],
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.scss'
 })
 export class EditorComponent {
   item = input();
+  steps: StepperItem[]=[];
+  constructor() {
+    this.initSteps();
+  }
+  initSteps(){
+    this.steps = [
+      {name: 'Requester Details'},
+      {name: 'Objectives'},
+      {name: 'Date'},
+      {name: 'Location'},
+      {name: 'Particulars'},
+      {name: 'Location'},
+      {name: 'Documents / Attachments'},
+      {name: 'Review'},
+    ];
+  }
 }
