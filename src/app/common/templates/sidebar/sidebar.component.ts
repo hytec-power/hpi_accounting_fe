ea229ff9ad0be410162eca1e0d0cb0302fca6afb
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-07 13:38:27 +0800

    common sidebar component

--- src/app/common/templates/sidebar/sidebar.component.ts
+++ src/app/common/templates/sidebar/sidebar.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import {ThemeService} from "src/app/services/theme/theme.service";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
-  selector: 'app-sidebar',
+  selector: 'common-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLinkActive,
+    RouterLink
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  items:{ name: string,link: string, bi_icon?: string , bi_icon_active?: string ; exact: boolean }[] = [];
+  toggle_temp: boolean = true;
+  constructor(private theme: ThemeService) {
+    this.items.push({name: 'Dashboard',
+      link: '/accounting',
+      bi_icon: 'bi-house',
+      bi_icon_active: 'bi-house-door-fill',
+      exact: true });
 
+    this.items.push({name: 'Budget Request',
+      link: '/accounting/budget-requests' ,
+      bi_icon: 'bi-wallet2',
+      bi_icon_active: 'bi-wallet-fill',
+      exact: false });
+  }
+  test(){
+    this.toggle_temp = !this.toggle_temp;
+    this.theme.setTheme(this.toggle_temp?'light':'dark');
+  }
 }
--- src/app/pages/accounting/common/template/template.component.ts
+++ src/app/pages/accounting/common/template/template.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import {SidebarComponent} from "src/app/pages/accounting/common/sidebar/sidebar.component";
 import {RouterOutlet} from "@angular/router";
 import {HeaderComponent} from "src/app/common/templates/header/header.component";
+import {SidebarComponent} from "src/app/common/templates/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-template',
   standalone: true,
     imports: [
-        SidebarComponent,
         RouterOutlet,
-        HeaderComponent
+        HeaderComponent,
+        SidebarComponent
     ],
   templateUrl: './template.component.html',
   styleUrl: './template.component.scss'
