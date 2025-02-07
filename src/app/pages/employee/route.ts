9a01a3b3e6defe047262f193848ef370b7c71e76
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-07 18:30:51 +0800

    create component placeholder

--- src/app/pages/employee/route.ts
+++ src/app/pages/employee/route.ts
@@ -2,6 +2,7 @@ import {Route} from "@angular/router";
 import { EmployeeDashboardComponent } from "src/app/pages/employee/dashboard/dashboard.component";
 import { IndexComponent  as BudgetRequestIndex } from "src/app/pages/employee/budget-requests/index/index.component";
 import { ViewComponent as BudgetRequestViewer  } from "src/app/pages/employee/budget-requests/view/view.component";
+import { CreateComponent as BudgetRequestCreate } from "src/app/pages/employee/budget-requests/create/create.component";
 
 
 export const EmployeeRoutes: Route[]=[
@@ -10,6 +11,9 @@ export const EmployeeRoutes: Route[]=[
     },{
         path: 'budget-requests',
         component: BudgetRequestIndex
+    },{
+        path: 'budget-requests/create',
+        component: BudgetRequestCreate
     },{
         path: 'budget-requests/:id',
         component: BudgetRequestViewer
