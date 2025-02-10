9d26634328e574e6efd21d5c8ef89b72e3094365
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-11 07:25:47 +0800

    particulars step

--- src/app/shared/budget-request/editor/request-particulars/request-particulars.component.ts
+++ src/app/shared/budget-request/editor/request-particulars/request-particulars.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {ButtonComponent} from "src/app/common/button/button.component";
+import {CurrencyPipe} from "@angular/common";
 
 @Component({
   selector: 'request-particulars',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonComponent,
+    CurrencyPipe
+  ],
   templateUrl: './request-particulars.component.html',
   styleUrl: './request-particulars.component.scss'
 })
