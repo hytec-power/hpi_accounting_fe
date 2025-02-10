a265a8f8bdcc0c8f73ff9515a682dbe8409ad7ab
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-11 07:46:15 +0800

    modal backdrop

--- src/app/shared/budget-request/editor/request-particulars/particulars-modal/particulars-modal.component.ts
+++ src/app/shared/budget-request/editor/request-particulars/particulars-modal/particulars-modal.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-particulars-modal',
+  selector: 'particulars-modal',
   standalone: true,
   imports: [],
   templateUrl: './particulars-modal.component.html',
   styleUrl: './particulars-modal.component.scss'
 })
 export class ParticularsModalComponent {
-
+  @ViewChild('modal',{static: true}) modal!: ElementRef;
+  constructor() {
+  }
+  ngOnInit(){
+    console.log(this.modal);
+  }
+  open(){
+    this.modal?.nativeElement.showModal();
+  }
 }
--- src/app/shared/budget-request/editor/request-particulars/request-particulars.component.ts
+++ src/app/shared/budget-request/editor/request-particulars/request-particulars.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {ButtonComponent} from "src/app/common/button/button.component";
 import {CurrencyPipe} from "@angular/common";
+import {
+  ParticularsModalComponent
+} from "src/app/shared/budget-request/editor/request-particulars/particulars-modal/particulars-modal.component";
 
 @Component({
   selector: 'request-particulars',
   standalone: true,
   imports: [
     ButtonComponent,
-    CurrencyPipe
+    CurrencyPipe,
+    ParticularsModalComponent
   ],
   templateUrl: './request-particulars.component.html',
   styleUrl: './request-particulars.component.scss'
 })
 export class RequestParticularsComponent {
-
+  @ViewChild('modal',{static: true , read: ParticularsModalComponent}) modal!: ParticularsModalComponent;
+  ngOnInit(){
+    this.onAdd();
+  }
+  onAdd(){
+    this.modal.open();
+  }
 }
