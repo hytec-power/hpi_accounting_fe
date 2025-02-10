4b16c240f03c8c27bfe574a58cb54eb2935c8cbd
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-11 07:31:26 +0800

    pariculars modal

---
+++ src/app/shared/budget-request/editor/request-particulars/particulars-modal/particulars-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-particulars-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './particulars-modal.component.html',
+  styleUrl: './particulars-modal.component.scss'
+})
+export class ParticularsModalComponent {
+
+}
