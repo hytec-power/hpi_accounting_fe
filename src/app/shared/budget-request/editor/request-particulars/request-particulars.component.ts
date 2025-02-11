4bdb5fef26460f34c581bb44a6a8c23940da50a2
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-11 09:13:51 +0800

    add expense itemn

--- src/app/shared/budget-request/editor/request-particulars/request-particulars.component.ts
+++ src/app/shared/budget-request/editor/request-particulars/request-particulars.component.ts
@@ -1,9 +1,10 @@
 import {Component, ViewChild} from '@angular/core';
 import {ButtonComponent} from "src/app/common/button/button.component";
-import {CurrencyPipe} from "@angular/common";
+import {CurrencyPipe, TitleCasePipe} from "@angular/common";
 import {
   ParticularsModalComponent
 } from "src/app/shared/budget-request/editor/request-particulars/particulars-modal/particulars-modal.component";
+import {ExpenseItem} from "src/app/interfaces/expense-item";
 
 @Component({
   selector: 'request-particulars',
@@ -11,17 +12,18 @@ import {
   imports: [
     ButtonComponent,
     CurrencyPipe,
-    ParticularsModalComponent
+    ParticularsModalComponent,
+    TitleCasePipe
   ],
   templateUrl: './request-particulars.component.html',
   styleUrl: './request-particulars.component.scss'
 })
 export class RequestParticularsComponent {
   @ViewChild('modal',{static: true , read: ParticularsModalComponent}) modal!: ParticularsModalComponent;
-  ngOnInit(){
-    this.onAdd();
-  }
-  onAdd(){
+  items: ExpenseItem[]=[];
+  ngOnInit(){}
+  addItem(){
     this.modal.open();
   }
+
 }
