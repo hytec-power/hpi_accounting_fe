c3bbf46a6f971026245ea086090ff8a436c6a706
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-11 08:54:30 +0800

    expense input form

--- src/app/shared/budget-request/editor/request-particulars/particulars-modal/particulars-modal.component.ts
+++ src/app/shared/budget-request/editor/request-particulars/particulars-modal/particulars-modal.component.ts
@@ -1,20 +1,38 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
+import {ButtonComponent} from "src/app/common/button/button.component";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'particulars-modal',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './particulars-modal.component.html',
   styleUrl: './particulars-modal.component.scss'
 })
 export class ParticularsModalComponent {
   @ViewChild('modal',{static: true}) modal!: ElementRef;
-  constructor() {
+  form!: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.initForm();
+
   }
-  ngOnInit(){
-    console.log(this.modal);
+  ngOnInit(){}
+  initForm(){
+    this.form = this.fb.group({
+        type: ['',[Validators.required]],
+        amount: [0,[Validators.required,
+                    Validators.min(0),
+                    Validators.pattern('^\\d+$'),
+                    Validators.max(999999999)]],
+    });
   }
   open(){
     this.modal?.nativeElement.showModal();
+  }
+  close(){
+    this.modal.nativeElement.close();
   }
 }
