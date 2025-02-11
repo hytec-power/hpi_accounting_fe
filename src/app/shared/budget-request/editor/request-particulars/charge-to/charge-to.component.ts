1d8ef4a4f4e37ed8847837f924b7976c10e07058
zach madrilejos <zach.madrilejos@gmail.com>
2025-02-11 12:19:14 +0800

    charge to component

--- src/app/shared/budget-request/editor/request-particulars/charge-to/charge-to.component.ts
+++ src/app/shared/budget-request/editor/request-particulars/charge-to/charge-to.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'charge-to',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './charge-to.component.html',
   styleUrl: './charge-to.component.scss'
 })
 export class ChargeToComponent {
+  form !: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.initForm();
+  }
+  ngOnInit(){
 
+  }
+  initForm(){
+    this.form = this.fb.group({
+      client_name           : ['',Validators.required],
+      po_quotation_reference: ['',Validators.required],
+      remaining_contingency : ['',Validators.required],
+      release_type          : ['',Validators.required],
+    });
+  }
 }
