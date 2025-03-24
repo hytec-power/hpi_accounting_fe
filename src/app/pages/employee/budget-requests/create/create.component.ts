aae1aa5b266798499f3706c44a179d0b36c08dd0
zachiees <zach.madrilejos@gmail.com>
2025-03-24 16:34:52 +0800

    request details placeholder

--- src/app/pages/employee/budget-requests/create/create.component.ts
+++ src/app/pages/employee/budget-requests/create/create.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {EditorComponent} from "src/app/shared/budget-request/editor/editor.component";
 import {PageTitleComponent} from "src/app/common/page-title/page-title.component";
+import {CreateComponent as CreateRequest} from "../../../../shared/budget-request/create/create.component";
 
 @Component({
     selector: 'app-create',
-    imports: [
-        EditorComponent,
-        PageTitleComponent
-    ],
+  imports: [
+    EditorComponent,
+    PageTitleComponent,
+    CreateRequest
+  ],
     templateUrl: './create.component.html',
     styleUrl: './create.component.scss'
 })
---
+++ src/app/shared/budget-request/create/request-details/request-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'request-details',
+  imports: [],
+  templateUrl: './request-details.component.html',
+  styleUrl: './request-details.component.scss'
+})
+export class RequestDetailsComponent {
+
+}
