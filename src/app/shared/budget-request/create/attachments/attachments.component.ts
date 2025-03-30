9c356c7a1794ec6e726b18e060dc3b62421881b2
ellychuliao <echuliao@gmail.com>
2025-03-30 19:44:07 +0800

    validationform

--- src/app/shared/budget-request/create/attachments/attachments.component.ts
+++ src/app/shared/budget-request/create/attachments/attachments.component.ts
@@ -8,5 +8,17 @@ import { ButtonComponent } from 'src/app/common/button/button.component';
   styleUrl: './attachments.component.scss'
 })
 export class AttachmentsComponent {
+  imagePreview: string | undefined;
 
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagePreview = reader.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+  
 }
--- src/app/shared/budget-request/create/request-details/request-details.component.ts
+++ src/app/shared/budget-request/create/request-details/request-details.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
-import {ButtonComponent} from "src/app/common/button/button.component";
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ButtonComponent } from 'src/app/common/button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'request-details',
-  imports: [
-    ButtonComponent
-  ],
+  standalone: true,
+  imports: [ButtonComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './request-details.component.html',
   styleUrl: './request-details.component.scss'
 })
 export class RequestDetailsComponent {
+  requestForm: FormGroup;
 
+  constructor(private check: FormBuilder) {
+    this.requestForm = this.check.group({
+      clientVisit: [false],
+      ocularVisit: [false],
+      clientMeeting: [false],
+      deliveryLocation: [false],
+      sdpPresentation: [false],
+      repair: [false],
+      proposalDiscussion: [false],
+      bidding: [false],
+      optimizationSurvey: [false],
+      commissioning: [false],
+      clientProspecting: [false],
+      eventExhibit: [false],
+    });
+  }
+
+  isAnyCheckboxChecked(): boolean {
+    return Object.values(this.requestForm.value).some(value => value === true);
+  }
 }
