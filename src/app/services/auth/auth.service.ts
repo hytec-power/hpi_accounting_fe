75cc1fd8f88115b5ee2e368bfda64d92e345a750
zachiees <zach.madrilejos@gmail.com>
2025-04-02 13:55:31 +0800

    auth state

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable, signal, WritableSignal} from '@angular/core';
 import {environment} from "src/environments/environment";
+import {AppStateService} from "src/app/services/app-state/app-state.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
    api: string = `${environment.apiUrl}/auth`;
-  constructor() { }
+   auth_authenticated: WritableSignal<boolean> = signal(false);
+  constructor(private appState: AppStateService) {
+    this.appState.registerState('auth_authenticated',this.auth_authenticated,false);
+  }
   getOauthUrl(){
     return `${environment.oauth_login_url}?client_key=${this.getOauthKey()}`
   }
