c9802142517bbccda2a05700baeb6ff2a6239f7b
zachiees <zach.madrilejos@gmail.com>
2025-04-02 08:08:05 +0800

    css cleanups

--- src/app/shared/budget-request/create/create.component.ts
+++ src/app/shared/budget-request/create/create.component.ts
@@ -1,11 +1,6 @@
 import {Component, input, signal, WritableSignal} from '@angular/core';
-import {StepperComponent, StepperItem} from "../../../common/stepper/stepper.component";
-import {ButtonComponent} from "../../../common/button/button.component";
-import {DateLocationComponent} from "../editor/date-location/date-location.component";
-import {RequestAttachmentsComponent} from "../editor/request-attachments/request-attachments.component";
-import {RequestObjectivesComponent} from "../editor/request-objectives/request-objectives.component";
-import {RequestParticularsComponent} from "../editor/request-particulars/request-particulars.component";
-import {RequesterDetailsComponent} from "../editor/requester-details/requester-details.component";
+import {StepperComponent, StepperItem} from "src/app/common/stepper/stepper.component";
+import {ButtonComponent} from "src/app/common/button/button.component";
 import {RequestDetailsComponent} from "src/app/shared/budget-request/create/request-details/request-details.component";
 import {DateTimeComponent} from "src/app/shared/budget-request/create/date-time/date-time.component";
 import { ChargingDetailsComponent } from './charging-details/charging-details.component';
@@ -18,11 +13,6 @@ import { RequestManpowerComponent } from "./request-manpower/request-manpower.co
   imports: [
     StepperComponent,
     ButtonComponent,
-    DateLocationComponent,
-    RequestAttachmentsComponent,
-    RequestObjectivesComponent,
-    RequestParticularsComponent,
-    RequesterDetailsComponent,
     RequestDetailsComponent,
     DateTimeComponent,
     ChargingDetailsComponent,
