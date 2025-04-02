c522409a28fde13935ef1678086891740a6f76f1
zachiees <zach.madrilejos@gmail.com>
2025-04-02 14:08:27 +0800

    oauth login

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,6 @@ import {Environment} from "src/app/interfaces/environment";
 export const environment: Environment = {
   oauth_login_url: "https://oauth.hpi-connect.e-hytec.dev/oauth",
   apiUrl: "",
-  oauth_client_key: "m238dnawek2347nmfgi123om1239",
+  oauth_client_key: "f6a357234b3dbcd160662d44a2874c141bc155dac6142e3ecf6ac36c7510e470",
   production: false
 };
