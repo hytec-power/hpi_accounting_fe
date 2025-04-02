0b8a4fc0814fa098f9956dd4d2eeb91f567be7f3
zachiees <zach.madrilejos@gmail.com>
2025-04-02 10:35:09 +0800

    auth login page

--- src/app/pages/public/routes.ts
+++ src/app/pages/public/routes.ts
@@ -1,9 +1,13 @@
 import { Route } from "@angular/router";
 import { IndexComponent } from "src/app/pages/public/index/index.component";
+import { AuthComponent } from "src/app/pages/public/auth/auth.component";
 
 export const PublicRoutes: Route[] = [
     {
         path: '',
         component: IndexComponent
-    }
+    },{
+    path: 'login',
+    component: AuthComponent
+  }
 ];
