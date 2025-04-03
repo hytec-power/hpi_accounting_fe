de6978977285af6e186ac044693622c208095c1c
zachiees <zach.madrilejos@gmail.com>
2025-04-03 15:03:18 +0800

    change token

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,6 @@ import {Environment} from "src/app/interfaces/environment";
 export const environment: Environment = {
   oauth_login_url: "https://oauth.hpi-connect.e-hytec.dev/oauth",
   apiUrl: "",
-  oauth_client_key: "7292ccc48e7e438fc8677e7ba79ace4d2c0d5f5e6d47331455ff0f3480a0f527",
+  oauth_client_key: "542e20083ea2a34cee98f5da8b49db50e3d5cd66179675aea5d2316bca8f8bca",
   production: false
 };
