edb1d01bace5f58fe29a53b9b334069c718f0832
zachiees <zach.madrilejos@gmail.com>
2025-04-03 15:27:47 +0800

    change keys

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,6 @@ import {Environment} from "src/app/interfaces/environment";
 export const environment: Environment = {
   oauth_login_url: "https://oauth.hpi-connect.e-hytec.dev/oauth",
   apiUrl: "",
-  oauth_client_key: "542e20083ea2a34cee98f5da8b49db50e3d5cd66179675aea5d2316bca8f8bca",
+  oauth_client_key: "08893f848d766cd9c04dd0e479da7a0c81b8efc61af61058be8b4398e0183994",
   production: false
 };
