3b77bc13ef39075dc0cc3e3d80aa440be8706657
zachiees <zach.madrilejos@gmail.com>
2025-04-05 15:31:22 +0800

    save current user

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -2,6 +2,8 @@ import {Injectable, signal, WritableSignal} from '@angular/core';
 import {environment} from "src/environments/environment";
 import {AppStateService} from "src/app/services/app-state/app-state.service";
 import {HttpClient} from "@angular/common/http";
+import {tap} from "rxjs";
+import {CurrentUser} from "src/app/interfaces/current-user";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,7 @@ import {HttpClient} from "@angular/common/http";
 export class AuthService {
    api: string = `${environment.apiUrl}/auth`;
    authenticated: WritableSignal<boolean> = signal(false);
-   current_user: WritableSignal<boolean> = signal(true);
+   current_user: WritableSignal<CurrentUser|null> = signal(null);
   constructor(private appState: AppStateService,
               private http: HttpClient) {
     this.appState.registerState('auth_state',this.authenticated,false);
@@ -21,12 +23,20 @@ export class AuthService {
     return environment.oauth_client_key
   }
   oauthLogin(token: string){
-    return this.http.post(`${this.api}/login`,{token: token},{ observe: 'response' })
+    return this.http
+               .post<CurrentUser>(`${this.api}/login`,{token: token},{ observe: 'body' })
+               .pipe( tap(data => this.setCurrentUser(data)));
   }
   getAuthState(){
     return this.authenticated;
   }
+  getCurrentUser(){
+    return this.current_user();
+  }
   isAuthenticated(){
     return this.authenticated();
+  }
+  setCurrentUser(user: CurrentUser){
+    this.current_user.set(user);
   }
 }
