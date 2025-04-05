30f5c335b8732a76423456b66cd1f6b011035412
zachiees <zach.madrilejos@gmail.com>
2025-04-05 19:56:19 +0800

    request details

--- src/app/shared/budget-request/create/request-details/request-details.component.ts
+++ src/app/shared/budget-request/create/request-details/request-details.component.ts
@@ -1,5 +1,5 @@
-import { Component} from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import {Component, input, model, output} from '@angular/core';
+import {FormGroup, ReactiveFormsModule} from '@angular/forms';
 import { ButtonComponent } from 'src/app/common/button/button.component';
 import { CommonModule } from '@angular/common';
 
@@ -11,8 +11,10 @@ import { CommonModule } from '@angular/common';
   styleUrl: './request-details.component.scss'
 })
 export class RequestDetailsComponent {
-
-  purpose: string[]=[
+  form = input.required<FormGroup>()
+  purpose = model<string[]>([]);
+  onNext = output();
+  request_purpose: string[]=[
     'Client Visit',
     'Client Meeting',
     'Sdp Presentation',
@@ -27,6 +29,19 @@ export class RequestDetailsComponent {
     'Commissioning And Installation ASI',
     'Event Exhibit Convention'
   ];
-  request_types: string[] = [ 'Bidding', 'Training', 'Event/Echibition', 'Bidding Documents', 'TCP', 'Sponsorship'];
+  request_types: string[] = [ 'Bidding', 'Training', 'Event/Exhibition', 'Bidding Documents', 'TCP', 'Sponsorship'];
   constructor() {}
+  toggleSelection(event: any,value: string) {
+
+    event.target.checked ? this.add(value): this.remove(value);
+  }
+  add(item:string) {
+    !this.purpose().includes(item) && this.purpose.update( i => [...i,item]);
+  }
+  remove(item:string) {
+    this.purpose.update( i => i.filter(a => a!==item));
+  }
+  isComplete(){
+    return this.form().valid && this.purpose().length > 0;
+  }
 }
