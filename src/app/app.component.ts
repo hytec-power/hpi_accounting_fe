8138dbef9bc2f04ed4312fc1dbc9b52c1aba1801
zachiees <zach.madrilejos@gmail.com>
2025-04-07 15:02:56 +0800

    pre load theme service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {AppStateService} from "src/app/services/app-state/app-state.service";
+import {ThemeService} from "src/app/services/theme/theme.service";
 
 @Component({
     selector: 'app-root',
@@ -10,6 +11,7 @@ import {AppStateService} from "src/app/services/app-state/app-state.service";
 })
 export class AppComponent {
   title = 'hpi_accounting_fe';
-  constructor(private appState: AppStateService) {
+  constructor(private appState: AppStateService,
+              private theme: ThemeService) {
   }
 }
