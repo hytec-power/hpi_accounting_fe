22b4b7206cf6271c5f0efbd58c8b446f15ee8de5
zachiees <zach.madrilejos@gmail.com>
2025-04-08 11:44:11 +0800

    budget request service

---
+++ src/app/services/employee/budget-reqeust/budget-request.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {environment} from "src/environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {BudgetRequest} from "src/app/interfaces/budget-request";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BudgetRequestService {
+  api = `${environment.apiUrl}/employee/budget-requests`;
+  constructor(private http: HttpClient) { }
+  index(){
+    return this.http.get<BudgetRequest[]>(`${this.api}`,{observe: 'body'});
+  }
+  find(uuid: string) {
+    return this.http.get<BudgetRequest>(`${this.api}/${uuid}`,{ observe: 'body' });
+  }
+  create(payload: any){
+    return this.http.post(`${this.api}`,{observe: 'response'});
+  }
+  update(uuid: string, payload: any){
+    return this.http.patch(`${this.api}/${uuid}`,{observe: 'response'});
+  }
+  delete(uuid: string){
+
+  }
+}
