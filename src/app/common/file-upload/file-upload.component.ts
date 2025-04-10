2d341d1f7fd18385725bd00cd6786a3f75d736ac
zachiees <zach.madrilejos@gmail.com>
2025-04-10 13:53:05 +0800

    file upload validatos

--- src/app/common/file-upload/file-upload.component.ts
+++ src/app/common/file-upload/file-upload.component.ts
@@ -1,4 +1,4 @@
-import {Component, input} from '@angular/core';
+import {Component, computed, input} from '@angular/core';
 
 @Component({
   selector: 'file-upload',
@@ -9,4 +9,10 @@ import {Component, input} from '@angular/core';
 export class FileUploadComponent {
   width_px = input<number>(427);
   height_px = input<number>(81);
+  extensions = input<string[]>([]);
+  accept = computed(()=>this.extensions().join(','));
+  max_size = input<number>(2097152);
+  onFileSelected(event: Event) {
+
+  }
 }
