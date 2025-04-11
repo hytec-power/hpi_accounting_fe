975c2b330a9576f31ea5edac007db0b7f968e2af
Jericho Christian Bautista Dalmace <jerichod37@Jerichos-MacBook-Air.local>
2025-04-11 11:36:22 +0800

    request-attachments necessary files for each request type

--- src/app/shared/budget-request/create/attachments/attachments.component.ts
+++ src/app/shared/budget-request/create/attachments/attachments.component.ts
@@ -15,7 +15,8 @@ import {FileUploadComponent} from "src/app/common/file-upload/file-upload.compon
 })
 export class AttachmentsComponent {
   //
-  request_type = input<string>('Bidding Documents');
+  request_type = input<string>('After Sales Training');
+  ast_type = input<string>('industrial');
   //NAVS
   onNext = output();
   onBack = output();
@@ -32,16 +33,64 @@ export class AttachmentsComponent {
       case 'Bidding Documents':
         this.requirements.push({ name: 'Intention to Bid' ,
                                  extensions: ['pdf','doc','docx','png','jpeg','jpg'],
-                                 size: 20971520,
+                                 size: 10485760,
                                  bi_icon : 'bi-file-earmark-fill' });
         break;
       case 'Training / Event / Exhibition':
+        this.requirements.push({ name: 'Approval letter' ,
+                                 extensions: ['pdf','doc','docx','png','jpeg','jpg'],
+                                 size: 10485760,
+                                 bi_icon : 'bi-file-earmark-fill' });
         break;
       case 'After Sales Training':
+        this.requirements.push({ name: 'Course outline' ,
+                                 extensions: ['pdf','doc','docx','png','jpeg','jpg'],
+                                 size: 20971520,
+                                 bi_icon : 'bi-file-earmark-fill' });
+        this.requirements.push({ name: 'Itinerary' ,
+                                 extensions: ['pdf','doc','docx','png','jpeg','jpg','xlx','xlsx'],
+                                 size: 20971520,
+                                 bi_icon : 'bi-file-earmark-fill' });
+        this.requirements.push({ name: 'List of Participants' ,
+                                 extensions: ['pdf','doc','docx','png','jpeg','jpg','xlx','xlsx'],
+                                 size: 10485760,
+                                 bi_icon : 'bi-file-earmark-fill' });
+        this.requirements.push({ name: 'Contingency Computation' ,
+                                 extensions: ['pdf','doc','docx','png','jpeg','jpg','xlx','xlsx'],
+                                 size: 20971520,
+                                 bi_icon : 'bi-file-earmark-fill' });
+        this.requirements.push({ name: 'MCC' ,
+                                 extensions: ['pdf','doc','docx','png','jpeg','jpg'],
+                                 size: 20971520,
+                                 bi_icon : 'bi-file-earmark-fill' });
+        if (this.ast_type() === 'industrial'){
+          this.requirements.push({ name: 'Purchase order' ,
+                                   extensions: ['pdf','doc','docx','png','jpeg','jpg'],
+                                   size: 20971520,
+                                   bi_icon : 'bi-file-earmark-fill' });
+          this.requirements.push({ name: 'Quotation Ref' ,
+                                 extensions: ['pdf','doc','docx','png','jpeg','jpg'],
+                                 size: 20971520,
+                                 bi_icon : 'bi-file-earmark-fill' });
+        }
+        if (this.ast_type() === 'Academe'){
+          this.requirements.push({ name: 'Course outline' ,
+                                   extensions: ['pdf','doc','docx','png','jpeg','jpg'],
+                                   size: 20971520,
+                                   bi_icon : 'bi-file-earmark-fill' });
+        }
         break;
       case 'TCP':
+        this.requirements.push({ name: 'Client request' ,
+                                 extensions: ['pdf','doc','docx','png','jpeg','jpg'],
+                                 size: 10485760,
+                                 bi_icon : 'bi-file-earmark-fill' });
         break;
       case 'Sponsorship':
+        this.requirements.push({ name: 'Client request letter' ,
+                                 extensions: ['pdf','doc','docx','png','jpeg','jpg'],
+                                 size: 10485760,
+                                 bi_icon : 'bi-file-earmark-fill' });
         break;
     }
   }
