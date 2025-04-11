9f98da68c061a9446f0da3fffaacf72fd46934b6
zachiees <zach.madrilejos@gmail.com>
2025-04-11 10:35:38 +0800

    attachment requirements

---
+++ src/app/shared/budget-request/create/attachments/file-requirement/file-requirement.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-requirement',
+  imports: [],
+  templateUrl: './file-requirement.component.html',
+  styleUrl: './file-requirement.component.scss'
+})
+export class FileRequirementComponent {
+
+}
