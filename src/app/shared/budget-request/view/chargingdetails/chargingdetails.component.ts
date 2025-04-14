279cb89b2eaf3598ac25b1240850d279de1f3024
ellychuliao <echuliao@gmail.com>
2025-04-14 14:32:24 +0800

    chargingdetail

--- src/app/shared/budget-request/view/chargingdetails/chargingdetails.component.ts
+++ src/app/shared/budget-request/view/chargingdetails/chargingdetails.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { BudgetRequest } from 'src/app/interfaces/budget-request';
 
 @Component({
   selector: 'app-chargingdetails',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chargingdetails.component.html',
   styleUrl: './chargingdetails.component.scss'
 })
 export class ChargingdetailsComponent {
   record = input.required<BudgetRequest>();
+  isEditable = false;
+  onedit(){
+    this.isEditable = !this.isEditable
+  }
 }
