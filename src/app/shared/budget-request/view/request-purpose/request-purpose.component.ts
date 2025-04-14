fc01bec5a9bc752e413a2d8cfce949553fe55202
zachiees <zach.madrilejos@gmail.com>
2025-04-14 09:08:02 +0800

    input sample

--- src/app/shared/budget-request/view/request-purpose/request-purpose.component.ts
+++ src/app/shared/budget-request/view/request-purpose/request-purpose.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {BudgetRequest} from "src/app/interfaces/budget-request";
+import {JsonPipe} from "@angular/common";
 
 @Component({
     selector: 'request-purpose',
-    imports: [],
+  imports: [],
     templateUrl: './request-purpose.component.html',
     styleUrl: './request-purpose.component.scss'
 })
 export class RequestPurposeComponent {
+  record = input.required<BudgetRequest>();
     purpose: any = [
         'Client Visit',
         'Occular Visit',
         'Faculty Immersion',
         'Delivery',
        ]
+  constructor() {}
+  ngOnInit() {}
 }
