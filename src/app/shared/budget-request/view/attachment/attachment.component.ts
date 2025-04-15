ac1bd4cbef503c908cdbb03fa06a5dfbf1c3b17f
ellychuliao <echuliao@gmail.com>
2025-04-15 11:18:33 +0800

    attachmentsedit

--- src/app/shared/budget-request/view/attachment/attachment.component.ts
+++ src/app/shared/budget-request/view/attachment/attachment.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-attachment',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './attachment.component.html',
   styleUrl: './attachment.component.scss'
 })
 export class AttachmentComponent {
-
+  isEditable = false;
+  onedit(){
+    this.isEditable = !this.isEditable
+  }
 }
