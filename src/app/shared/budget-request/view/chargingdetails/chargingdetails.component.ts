1d89cc2cc3e710856038f16fe49698b2fe7ec611
echooo <jrchchrstn@gmail.com>
2025-04-21 17:49:52 +0800

    charging details form validation

--- src/app/shared/budget-request/view/chargingdetails/chargingdetails.component.ts
+++ src/app/shared/budget-request/view/chargingdetails/chargingdetails.component.ts
@@ -2,15 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { BudgetRequest } from 'src/app/interfaces/budget-request';
 import { ButtonComponent } from "../../../../common/button/button.component";
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chargingdetails',
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, ReactiveFormsModule],
   templateUrl: './chargingdetails.component.html',
   styleUrl: './chargingdetails.component.scss'
 })
 export class ChargingdetailsComponent {
-  record = input.required<BudgetRequest>();
+  form  = input.required<FormGroup>();
   isEditable = false;
   onedit(){
     this.isEditable = !this.isEditable
