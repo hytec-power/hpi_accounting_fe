88f07619847b771afe72e004f3b4792c1654d8a0
echooo <jrchchrstn@gmail.com>
2025-04-22 11:18:22 +0800

    view/attachments fix

--- src/app/shared/budget-request/view/attachment/attachment.component.ts
+++ src/app/shared/budget-request/view/attachment/attachment.component.ts
@@ -1,18 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { Component, input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, input, model } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BudgetRequest } from 'src/app/interfaces/budget-request';
+import { FileSizePipe } from './file-size.pipe';
 
 @Component({
   selector: 'app-attachment',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, 
+        CommonModule, 
+        FileSizePipe, 
+        ReactiveFormsModule],
   templateUrl: './attachment.component.html',
   styleUrl: './attachment.component.scss'
 })
 export class AttachmentComponent {
   record = input.required<BudgetRequest>();
   isEditable = false;
+  ngOnInit(){
+  }
   onedit(){
     this.isEditable = !this.isEditable
   }
 }
+
