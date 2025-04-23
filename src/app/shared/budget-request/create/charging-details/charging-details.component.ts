6896cc26f7917a9033d8a3dc0e9c7bfbdf8b4a04
zachiees <zach.madrilejos@gmail.com>
2025-04-23 21:53:34 +0800

    load clients

--- src/app/shared/budget-request/create/charging-details/charging-details.component.ts
+++ src/app/shared/budget-request/create/charging-details/charging-details.component.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
-import {Component, input, output} from '@angular/core';
+import {Component, input, model, output} from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ButtonComponent } from 'src/app/common/button/button.component';
+import {Client} from "src/app/interfaces/client";
+import {ClientsService} from "src/app/services/common/clients/clients.service";
+import {DropdownItem} from "src/app/common/dropdown/dropdown.component";
 
 @Component({
   selector: 'charging-details',
@@ -13,11 +16,21 @@ export class ChargingDetailsComponent {
   //DATA
   form = input.required<FormGroup>()
   //
+  items: Client[]=[];
+  dropdown_clients: DropdownItem[]=[];
   onNext = output();
   onBack = output();
   list_years: number[]=[];
-  constructor(){
+  selected_client = model<Client|null>(null);
+  constructor(private client: ClientsService){
     this.init();
+  }
+  ngOnInit() {
+    this.apiFetch();
+  }
+  apiFetch(){
+    this.client.index()
+         .subscribe(res=>this.items = res);
   }
   init(){
     this.initYears();
