f69e6fa74e0b589375768bb49036ef3b58bcf493
zachiees <zach.madrilejos@gmail.com>
2025-04-25 07:49:39 +0800

    code cleanups

--- src/app/pages/accounting/routes.ts
+++ src/app/pages/accounting/routes.ts
@@ -2,7 +2,6 @@ import { Route } from "@angular/router";
 import {DashboardComponent} from "src/app/pages/accounting/dashboard/dashboard.component";
 import { ViewComponent as ViewBudgetRequest } from "src/app/pages/accounting/budget-requests/view/view.component";
 import { IndexComponent as BudgetRequestIndex } from "src/app/pages/accounting/budget-requests/index/index.component";
-import { ListschoolcompanyComponent } from "./listschoolcompany/listschoolcompany.component";
 import {ClientsComponent} from "src/app/pages/accounting/clients/clients.component";
 
 export const AccountingRoutes: Route[]=[
@@ -14,9 +13,6 @@ export const AccountingRoutes: Route[]=[
     },{
         path: 'budget-requests/:uuid',
         component: ViewBudgetRequest
-    },{
-        path: 'listschoolcompany',
-        component: ListschoolcompanyComponent
     },{
         path: 'clients',
         component: ClientsComponent
