75e217164c734e4cfc52eb9a01f2a1ca08f7d00b
zach madrilejos <zach.madrilejos@gmail.com>
2025-04-25 15:29:16 +0800

    cleanups

--- src/app/shared/budget-request/view/request-date/request-date.component.ts
+++ src/app/shared/budget-request/view/request-date/request-date.component.ts
@@ -3,12 +3,14 @@ import { ButtonComponent } from 'src/app/common/button/button.component';
 import { CommonModule, DatePipe, NgClass } from '@angular/common';
 import { BudgetRequest } from 'src/app/interfaces/budget-request';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {LoaderSpinnerComponent} from "src/app/common/loader-spinner/loader-spinner.component";
 @Component({
     selector: 'request-date',
     imports: [
         ButtonComponent,
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        LoaderSpinnerComponent
     ],
     templateUrl: './request-date.component.html',
     styleUrl: './request-date.component.scss'
@@ -16,6 +18,7 @@ import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/
 export class RequestDateComponent {
     form!: FormGroup;
     isEditable:boolean = false
+    loading: boolean = false;
     data = model.required<BudgetRequest>()
     constructor(private fb: FormBuilder) {
       this.initForm();
@@ -33,7 +36,11 @@ export class RequestDateComponent {
     loadData(){
         this.form.patchValue(this.data());
     }
-    toggleEdit(){
-        this.isEditable = !this.isEditable
+    cancel(){
+        this.loadData();
+        this.isEditable = false;
+    }
+    save(){
+
     }
 }
