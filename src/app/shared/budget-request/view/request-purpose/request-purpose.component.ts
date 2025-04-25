8884efff8bda10507697df7d951833c2e07c2bee
zach madrilejos <zach.madrilejos@gmail.com>
2025-04-25 14:22:33 +0800

    request purpose revisions

--- src/app/shared/budget-request/view/request-purpose/request-purpose.component.ts
+++ src/app/shared/budget-request/view/request-purpose/request-purpose.component.ts
@@ -1,4 +1,4 @@
-import {Component, input} from '@angular/core';
+import {Component, input, model} from '@angular/core';
 import {BudgetRequest} from "src/app/interfaces/budget-request";
 import {CommonModule, JsonPipe} from "@angular/common";
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -16,13 +16,38 @@ import { ButtonComponent } from "../../../../common/button/button.component";
     styleUrl: './request-purpose.component.scss'
 })
 export class RequestPurposeComponent {
-  form = input.required<FormGroup>();
+  data = model.required<BudgetRequest>();
+  isEditable: boolean = false;
+  type!: string;
+  purpose: string[]=[];
+  request_purpose: string[]=['Client Visit',
+                             'Client Meeting',
+                             'Sdp Presentation',
+                             'Proposal Discussion',
+                             'Optimization Survey',
+                             'Client Prospecting',
+                             'Faculty Immersion',
+                             'Ocular Visit',
+                             'Delivery Location',
+                             'Repair',
+                             'Bidding',
+                             'Commissioning And Installation ASI',
+                             'Event Exhibit Convention'];
+  request_types: string[] = [ 'Bidding Documents', 'Training / Event / Exhibition', 'After Sales Training', 'TCP', 'Sponsorship'];
+  constructor() {
+    this.initForm();
+  }
+  initForm(){
 
-  constructor() {}
-  ngOnInit() {}
-  
-  isEditable = false;
-  onedit(){
-    this.isEditable = !this.isEditable
+  }
+  ngOnInit() {
+      this.loadData();
+  }
+  loadData(){
+      this.type = this.data().type;
+      this.purpose = this.data().purpose;
+  }
+  toggleEdit(){
+      this.isEditable = !this.isEditable;
   }
 }
