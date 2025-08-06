5c706b988ec5d1eed222305d4bb237e3f1375934
Zach Madrilejos <zach.madrilejos@gmail.com>
2025-08-06 23:56:37 +0800

    PO page

--- src/app/pages/fae/purchase-orders/index/index.component.ts
+++ src/app/pages/fae/purchase-orders/index/index.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {ButtonComponent} from "src/app/common/button/button.component";
 
 @Component({
   selector: 'app-index',
-  imports: [],
+  imports: [
+    ButtonComponent
+  ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
