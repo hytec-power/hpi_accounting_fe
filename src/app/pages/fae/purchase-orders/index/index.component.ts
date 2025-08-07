794ad255c13655007c5894db5e5c035a5fcf8dc6
zach madrilejos <zach.madrilejos@gmail.com>
2025-08-07 19:50:37 +0800

    page titles

--- src/app/pages/fae/purchase-orders/index/index.component.ts
+++ src/app/pages/fae/purchase-orders/index/index.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {ButtonComponent} from "src/app/common/button/button.component";
 import {RouterLink} from "@angular/router";
+import {PageTitleComponent} from "src/app/common/page-title/page-title.component";
 
 @Component({
   selector: 'app-index',
   imports: [
     ButtonComponent,
-    RouterLink
+    RouterLink,
+    PageTitleComponent
   ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
