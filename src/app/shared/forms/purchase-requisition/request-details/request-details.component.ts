c02bc08197bfcc6840b190a294eb959692df3c9b
Zach Madrilejos <zach.madrilejos@gmail.com>
2025-08-07 21:32:17 +0800

    request details

--- src/app/shared/forms/purchase-requisition/request-details/request-details.component.ts
+++ src/app/shared/forms/purchase-requisition/request-details/request-details.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, input, output} from '@angular/core';
+import {FormGroup} from "@angular/forms";
+import {ButtonComponent} from "src/app/common/button/button.component";
 
 @Component({
   selector: 'pr-request-details',
-  imports: [],
+  imports: [
+    ButtonComponent
+  ],
   templateUrl: './request-details.component.html',
   styleUrl: './request-details.component.scss'
 })
 export class RequestDetailsComponent {
-
+  next = output<void>();
+  back = output<void>();
+  form = input<FormGroup>();
 }
