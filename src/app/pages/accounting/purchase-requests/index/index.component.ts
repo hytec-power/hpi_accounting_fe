d7fc0fe482fbd0a2d02f1387610ac7539a4c43ff
zach madrilejos <zach.madrilejos@gmail.com>
2025-08-11 01:35:04 +0800

    accounting PR

--- src/app/pages/accounting/purchase-requests/index/index.component.ts
+++ src/app/pages/accounting/purchase-requests/index/index.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import {ButtonComponent} from "src/app/common/button/button.component";
+import {CurrencyPipe, DatePipe} from "@angular/common";
+import {PageTitleComponent} from "src/app/common/page-title/page-title.component";
+import {PaginatorComponent} from "src/app/common/paginator/paginator.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-index',
-  imports: [],
+  imports: [
+    ButtonComponent,
+    CurrencyPipe,
+    DatePipe,
+    PageTitleComponent,
+    PaginatorComponent,
+    RouterLink
+  ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
 export class IndexComponent {
-
+  today = new Date();
 }
