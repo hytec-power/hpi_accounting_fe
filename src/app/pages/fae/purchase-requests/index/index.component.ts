8316be36c1ba1408f52dbbcec71053d287f35bb7
Zach Madrilejos <zach.madrilejos@gmail.com>
2025-08-10 19:05:58 +0800

    paginator

--- src/app/pages/fae/purchase-requests/index/index.component.ts
+++ src/app/pages/fae/purchase-requests/index/index.component.ts
@@ -2,17 +2,22 @@ import { Component } from '@angular/core';
 import {ButtonComponent} from "src/app/common/button/button.component";
 import {PageTitleComponent} from "src/app/common/page-title/page-title.component";
 import {RouterLink} from "@angular/router";
+import {CurrencyPipe, DatePipe} from "@angular/common";
+import {PaginatorComponent} from "src/app/common/paginator/paginator.component";
 
 @Component({
   selector: 'app-index',
   imports: [
     ButtonComponent,
     PageTitleComponent,
-    RouterLink
+    RouterLink,
+    CurrencyPipe,
+    DatePipe,
+    PaginatorComponent
   ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
 export class IndexComponent {
-
+  today = new Date();
 }
