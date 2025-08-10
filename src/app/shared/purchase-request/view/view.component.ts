29aaf1848cef8e7ddf1e49acae43261c467e7f19
Zach Madrilejos <zach.madrilejos@gmail.com>
2025-08-11 00:30:07 +0800

    view pr stepper

--- src/app/shared/purchase-request/view/view.component.ts
+++ src/app/shared/purchase-request/view/view.component.ts
@@ -3,6 +3,7 @@ import {ClientComponent} from "src/app/shared/purchase-request/view/client/clien
 import {FaeComponent} from "src/app/shared/purchase-request/view/fae/fae.component";
 import {ProjectDetailsComponent} from "src/app/shared/purchase-request/view/project-details/project-details.component";
 import {ItemsComponent} from "src/app/shared/purchase-request/view/items/items.component";
+import {StepperComponent, StepperItem} from "src/app/common/stepper/stepper.component";
 
 @Component({
   selector: 'pr-view',
@@ -10,11 +11,20 @@ import {ItemsComponent} from "src/app/shared/purchase-request/view/items/items.c
     ClientComponent,
     FaeComponent,
     ProjectDetailsComponent,
-    ItemsComponent
+    ItemsComponent,
+    StepperComponent
   ],
   templateUrl: './view.component.html',
   styleUrl: './view.component.scss'
 })
 export class ViewPrComponent {
-
+  steps: StepperItem[]=[];
+  constructor() {
+    this.initSteps();
+  }
+  initSteps(){
+    this.steps.push({name: "FAE", bi_icon: "bi-check-circle-fill"});
+    this.steps.push({name: "Intercom", bi_icon: "bi-check-circle-fill"});
+    this.steps.push({name: "Accounting", bi_icon: "bi-check-circle-fill"});
+  }
 }
