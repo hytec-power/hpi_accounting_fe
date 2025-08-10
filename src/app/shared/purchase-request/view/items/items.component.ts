58c5316f97197854fda0efbfaf7eb6eea290dee9
Zach Madrilejos <zach.madrilejos@gmail.com>
2025-08-10 23:13:18 +0800

    PR view items

--- src/app/shared/purchase-request/view/items/items.component.ts
+++ src/app/shared/purchase-request/view/items/items.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-items',
+  selector: 'pr-items',
   imports: [],
   templateUrl: './items.component.html',
   styleUrl: './items.component.scss'
--- src/app/shared/purchase-request/view/view.component.ts
+++ src/app/shared/purchase-request/view/view.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {ClientComponent} from "src/app/shared/purchase-request/view/client/client.component";
 import {FaeComponent} from "src/app/shared/purchase-request/view/fae/fae.component";
 import {ProjectDetailsComponent} from "src/app/shared/purchase-request/view/project-details/project-details.component";
+import {ItemsComponent} from "src/app/shared/purchase-request/view/items/items.component";
 
 @Component({
   selector: 'pr-view',
   imports: [
     ClientComponent,
     FaeComponent,
-    ProjectDetailsComponent
+    ProjectDetailsComponent,
+    ItemsComponent
   ],
   templateUrl: './view.component.html',
   styleUrl: './view.component.scss'
