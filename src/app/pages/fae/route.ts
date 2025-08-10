c16e5bf8ec07922e2cbf97064074b392daf1139f
Zach Madrilejos <zach.madrilejos@gmail.com>
2025-08-10 18:30:39 +0800

    PR section

--- src/app/pages/fae/route.ts
+++ src/app/pages/fae/route.ts
@@ -1,9 +1,15 @@
 import {Route} from "@angular/router";
 import { POIndexComponent } from "src/app/pages/fae/purchase-orders/index/index.component";
+import { IndexComponent  as PrIndex} from "src/app/pages/fae/purchase-requests/index/index.component";
 import { CreateComponent as CreatePR} from "src/app/pages/fae/purchase-orders/create/create.component";
 
 
 export const FaeRoutes: Route[] = [
+  {
+    path: 'purchase-request',
+    component: PrIndex,
+    title: 'Purchase Requests',
+  },
   {
     path: 'purchase-orders',
     component: POIndexComponent,
