e9ae253cecb92bb0ac3cfdb32ca1db79661959e9
Zach Madrilejos <zach.madrilejos@gmail.com>
2025-08-13 22:17:19 +0800

    env fix

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -4,6 +4,7 @@ export const environment: Environment = {
   oauth_login_url: "https://accounts.google.com/o/oauth2/v2/auth",
   oauth_client_key: "08893f848d766cd9c04dd0e479da7a0c81b8efc61af61058be8b4398e0183994",
   oauth_redirect_url: "",
+  oauth_client_id: "",
   apiUrl: "http://localhost:8000/api",
   production: false
 };
