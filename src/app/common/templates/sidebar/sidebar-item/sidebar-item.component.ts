159b3c3ea13002d9c9949fa0892e9f03021788a0
zach madrilejos <zach.madrilejos@gmail.com>
2025-08-13 11:06:06 +0800

    sidebar item component

---
+++ src/app/common/templates/sidebar/sidebar-item/sidebar-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-item',
+  imports: [],
+  templateUrl: './sidebar-item.component.html',
+  styleUrl: './sidebar-item.component.scss'
+})
+export class SidebarItemComponent {
+
+}
