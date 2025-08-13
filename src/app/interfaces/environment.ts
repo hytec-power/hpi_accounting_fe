c4e0a7db6644eadb05589fac191990b8502abacc
Zach Madrilejos <zach.madrilejos@gmail.com>
2025-08-13 22:15:38 +0800

    environments udpate

--- src/app/interfaces/environment.ts
+++ src/app/interfaces/environment.ts
@@ -3,4 +3,6 @@ export interface Environment {
   apiUrl:string;
   oauth_login_url:string;
   oauth_client_key: string
+  oauth_redirect_url: string
+  oauth_client_id: string
 }
--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,8 +1,9 @@
 import {Environment} from "src/app/interfaces/environment";
 
 export const environment: Environment = {
-  oauth_login_url: "https://oauth.hpi-connect.e-hytec.dev/oauth",
-  apiUrl: "http://localhost:8000/api",
+  oauth_login_url: "https://accounts.google.com/o/oauth2/v2/auth",
   oauth_client_key: "08893f848d766cd9c04dd0e479da7a0c81b8efc61af61058be8b4398e0183994",
+  oauth_redirect_url: "",
+  apiUrl: "http://localhost:8000/api",
   production: false
 };
