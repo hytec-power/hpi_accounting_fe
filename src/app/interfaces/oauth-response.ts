8013df4ab213319bf7593647b05b3ebeda396dc0
zach madrilejos <zach.madrilejos@gmail.com>
2025-08-14 01:07:11 +0800

    oauh response

---
+++ src/app/interfaces/oauth-response.ts
@@ -0,0 +1,5 @@
+export interface OauthResponse {
+    "access_token": string,
+    "token_type": string,
+    "expires_in": string,
+}
--- src/app/pages/public/oauth/oauth.component.ts
+++ src/app/pages/public/oauth/oauth.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {LoaderBouncingBallsComponent} from "src/app/common/loader-bouncing-balls/loader-bouncing-balls.component";
 import {AuthService} from "src/app/services/auth/auth.service";
 import * as qs from "qs";
+import {OauthResponse} from "src/app/interfaces/oauth-response";
 
 @Component({
   selector: 'app-oauth',
@@ -15,14 +16,12 @@ import * as qs from "qs";
 export class OauthComponent {
   token: string = '';
   access_token: string = '';
-  oauth_response: any = {};
+  oauth_response!: OauthResponse;
   constructor(private ac: ActivatedRoute,
               private router: Router,
               private auth: AuthService) {
     this.token = this.ac.snapshot.queryParamMap.get('token')?? '';
-    this.oauth_response = qs.parse(this.ac.snapshot.fragment??'');
-    console.log(this.oauth_response);
-
+    this.oauth_response = new Object(qs.parse(this.ac.snapshot.fragment??'')) as OauthResponse;
 
   }
   ngOnInit() {
@@ -31,11 +30,6 @@ export class OauthComponent {
       console.error('Oauth Error');
       return;
     }
-    // if(!this.token){
-    //   console.error('No access token provided.');
-    //   return;
-    // }
-    // this.apiLogin(this.token)
   }
   apiLogin(token: string){
     this.auth.oauthLogin(token).subscribe({
