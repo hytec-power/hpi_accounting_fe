5ba3720a9da3ed7a69a23f93a5dd8c2a4ffd3cd9
zach madrilejos <zach.madrilejos@gmail.com>
2025-08-20 18:46:58 +0800

    modal form controls

--- src/app/pages/admin/users/index/add/add.component.ts
+++ src/app/pages/admin/users/index/add/add.component.ts
@@ -1,17 +1,28 @@
 import {Component, ElementRef, viewChild} from '@angular/core';
 import {ButtonComponent} from "src/app/common/button/button.component";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'add-user',
   imports: [
-    ButtonComponent
+    ButtonComponent,
+    ReactiveFormsModule,
+    FormsModule
   ],
   templateUrl: './add.component.html',
   styleUrl: './add.component.scss'
 })
 export class AddComponent {
   modal = viewChild.required<ElementRef>('modal');
-  constructor() {
+  form!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.initForm();
+  }
+  initForm(){
+    this.form = this.formBuilder.group({
+      email: ['',[Validators.required]],
+      role: ['',[Validators.required]],
+    });
   }
   ngOnInit() {
 
